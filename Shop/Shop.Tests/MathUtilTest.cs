5df367da4772bd2890bcb5f2a2a921bc8f664c66
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2022-02-17 21:05:19 +0200

    simple unit test

---
+++ Shop/Shop.Tests/MathUtilTest.cs
@@ -0,0 +1,35 @@
+﻿using Shop.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Shop.Tests
+{
+    public class MathUtilTest
+    {
+        [Fact]
+        public void FactorialShouldReturnCorrectForFive()
+        {
+            //Arrange
+            //Act
+            var result = MathUtil.Factorial(5);
+
+            //Assert
+            Assert.Equal(120, result);
+        }
+
+        [Fact]
+        public void FactorialShouldReturnCorrectForOne()
+        {
+            //Arrange
+            //Act
+            var result = MathUtil.Factorial(1);
+
+            //Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ Shop/Shop/Utils/MathUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Utils
+{
+    public static class MathUtil
+    {
+        public static int Factorial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
